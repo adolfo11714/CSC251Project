289ca459d09627cb6c5e47e310bb63aed19ca75d
adolfo11714 <adolfomartinez122@yahoo.com>
2023-05-25 13:33:15 -0400

    no arg constructors

---
+++ Policy.java
@@ -0,0 +1,19 @@
+public class Policy {
+int id;
+String firstName;
+String lastName;
+int age;
+String smokingStatus;
+double height;
+double weight;
+
+public Policy() {
+id = 0000;
+firstName = "first name";
+lastName = "last name";
+age = 0;
+smokingStatus = "A";
+height = 0.0;
+weight = 0.0;
+}
+}
---
+++ Project_Adolfo_Martinez.java
@@ -0,0 +1,7 @@
+//demo class
+public class Project_Adolfo_Martinez{
+   public static void main(String[] args) {
+   Policy p = new Policy();
+   System.out.print(p.smokingStatus);
+   }
+}
