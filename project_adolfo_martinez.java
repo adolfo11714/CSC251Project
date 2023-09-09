9259f705a88898879639f9f4f6802fb1c345abeb
adolfo11714 <adolfomartinez122@yahoo.com>
2023-09-09 14:53:22 -0400

    little update
    added a couple more comments

--- project_adolfo_martinez.java
+++ project_adolfo_martinez.java
@@ -7,7 +7,7 @@ public class project_adolfo_martinez {
       String provider, fname, lname, smokingStatus; //provider name, first name, last name, smoking status
       double height, weight; //height(in inches), weight(in pounds)
 
-      
+      //user input
       Scanner keyboard = new Scanner(System.in); //initializing scanner
       System.out.print("Please enter the Policy Number: ");
       number = keyboard.nextInt();
@@ -38,7 +38,7 @@ public class project_adolfo_martinez {
       
       Policy p = new Policy(number, age, provider, fname, lname, smokingStatus, height, weight); //initializing policy class to access its data (getters)
       
-      System.out.println("-------------------------------------------------------"); //separator for the program to look more spaced out
+      System.out.println("-------------------------------------------------------"); //separator for the program to look more spaced out and neater
       
       p.displayUser(); //displays the info the user input into the program (output)
    }
